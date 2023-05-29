896a14c4d8e30c79518b978142bd2ef4531686bf
GoldEEr17 <108426205+GoldEEr17@users.noreply.github.com>
2023-05-29 15:24:05 +0200

    Update reseau_general.py

--- reseau_general.py
+++ reseau_general.py
@@ -5,19 +5,24 @@ from time import time
 
 
 class Reseau():
-    def __init__(self,taille:[int]):
+    def __init__(self,taille:[int],act_function='sigmoid'):
         self.taille = taille
-        # self.poids = [[]] + [  [ np.ones((taille[i],taille[i-1])),np.ones((taille[i],1)) ]  for i in range(1,len(taille))  ]
+        self.act_function = act_function
         self.poids = [[]] + [ [ np.random.uniform(-3,3,(taille[i],taille[i-1])),  np.random.uniform(-2,2,(taille[i],1))  ]      for i in range(1,len(self.taille)) ]
         self.accuracy = -1
         self.total_generations = 0
 
 
-    def sigmoid(self,t):
-        return 1 / (1+np.exp(-t))
+    def activation_function(self,t):
+        if self.act_function == 'sigmoid' :
+            return 1 / (1+np.exp(-t))
+
+    def der_activation_function(self,t):
+        if self.act_function == 'sigmoid' :
+            return np.exp(-t) / (1+np.exp(-t))**2
 
     def shuffled_data(self,DATA,data_shrink,data_number):
-        nb_elements = min(data_number, data_shrink*len(DATA),)
+        nb_elements = min(data_number, data_shrink*len(DATA))
         return np.random.sample(DATA,nb_elements)
 
     def evaluate_accuracy(self,data):
@@ -31,27 +36,61 @@ class Reseau():
 
 
     def feedforward(self,A_K:[float],J:int): # entrée A_K : vecteur colonne des activations de la couche K, précédent la couche suivante J, on va jusqu'à la dernière couche récursivement
-        if len(A_K.shape) == 1 : # s'assure que A_K soit bien un vecteur colonne et non une liste pour éviter les soucis
-            A_K = A_K[:,np.newaxis]
+        A_K = np.reshape(A_K,(len(A_K),1)) # transforme assurémenr en vecteur colonne
         if J == len(self.taille) :
             return A_K
         else :
             W_J = self.poids[J][0]
             B_J = self.poids[J][1]
             Z_J = np.dot(W_J, A_K) + B_J #Commentaire : le produit matrice/vecteur se fait que A_K soit un vecteur colonne ou une liste isomorphe, pas de pb après
-            A_J = self.sigmoid(Z_J)
+            A_J = self.activation_function(Z_J)
 
             self.feedforward(A_J,J+1)
+
+    def all_activations(self,entree):
+        entree = np.reshape(entree,(len(entree),1))
+        A_K, J, all_A,all_Z = entree, 1, [entree], [[]]
+        while J < len(self.taille) :
+            W_J = self.poids[J][0]
+            B_J = self.poids[J][1]
+            Z_J = np.dot(W_J,A_K) + B_J  # Commentaire : le produit matrice/vecteur se fait que A_K soit un vecteur colonne ou une liste isomorphe, pas de pb après
+            A_K = self.activation_function(Z_J)
+
+            all_A.append(A_K)
+            all_Z.append(Z_J)
+            J += 1
+        return [all_A,all_Z]
+
+
 
 
     def gradient_local(self,entree,sortie_attendue):
         partial_derivatives = np.zeros_like(self.poids)
+        all_A, all_Z = self.all_activations(entree)
+        #partial derivatives_A
+        sortie = all_A[-1]
 
-
-        def backpropagation(ratio_A_J,K):
-            if K == -1 :
+        par_der_A_S = [ 2 * (sortie[k] - sortie_attendue[k])**2   for k in range(len(sortie))   ]
+        def backpropagation(par_der_A_J,J):
+            if J == 0 :
                 pass
             else :
+                par_der_A_K = np.zeros_like(all_A[J-1])
+                for j in range(self.taille[J]) :
+                    par_der_A_K += par_der_A_J[j] * self.der_activation_function(all_Z[J][j] * self.poids[J][0][j,:].T)
+
+                # Biais : b_j0 = drond A_j0 * sigma' z_j0 * 1
+                partial_derivatives[J][1] += par_der_A_J * self.der_activation_function(all_Z[J]) * 1
+                # Poids.T
+                for j_row
+                    partial_derivatives[J][0] +=  [j] * self.der_activation_function(all_Z[j]) * all_A[J-1].T    for j in range(self.taille[J]) for k in range(self.taille[J-1])
+
+
+
+
+                backpropagation(par_der_A_K,J-1)
+
+
 
 
 
@@ -97,6 +136,7 @@ my_entree = np.array([0.7,0.4,0.6,0.8,0.1,0.1])
 # print(my_entree.shape)
 
 print(my_reseau.feedforward(my_entree,1))
+print(my_reseau.all_activations(my_entree))
 
 # my_reseau.apprendre([True,False,False],100.5)
 
