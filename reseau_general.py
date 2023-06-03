bbffc2ee2924881d06e0ee1107f04d168a573122
GoldEEr17 <108426205+GoldEEr17@users.noreply.github.com>
2023-06-03 12:00:38 +0200

    Update reseau_general.py

--- reseau_general.py
+++ reseau_general.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 from time import time
-
+import random as random
 
 
 class Reseau():
@@ -21,22 +21,43 @@ class Reseau():
         if self.act_function == 'sigmoid' :
             return np.exp(-t) / (1+np.exp(-t))**2
 
-    def shuffled_data(self,DATA,data_shrink,data_number):
-        nb_elements = min(data_number, data_shrink*len(DATA))
-        return np.random.sample(DATA,nb_elements)
+    def normalized(self,DATA, for_data_set=True):
+        if for_data_set :
+            if not isinstance(DATA[0][0], list) : return DATA
 
-    def evaluate_accuracy(self,data):
+            for i in range(len(DATA)) :
+                DATA[i][0] = np.reshape(DATA[i][0],(-1,1))
+                DATA[i][1] = np.reshape(DATA[i][1],(-1,1))
+
+            return DATA
+
+    def shuffled_data(self,DATA,data_shrink,data_number):
+        nb_elements = min(data_number, data_shrink*len(DATA),)
+        return random.sample(DATA,nb_elements)
+
+    #à corriger
+    def evaluate_accuracy(self,data,do_print=False):
+        data = self.normalized(data)
         for entree, sortie_attendue in data:
             sortie = self.feedforward(entree)
             correct_results = 0
-            if max(range(len(sortie)), key=lambda i: sortie[i]) == max(range(len(sortie_attendue)), key=lambda i: sortie_attendue[i]):
+            if max(range(sortie.size), key=lambda i: sortie[i]) == max(range(sortie_attendue.size), key=lambda i: sortie_attendue[i]):
                 correct_results += 1
         self.accuracy = correct_results / len(data)
+        if do_print==True : print(self.accuracy)
+
+    def zeros_like_recursive(self,First):
+        if isinstance(First,list):
+            return [self.zeros_like_recursive(elem) for elem in First]
+        if isinstance(First,np.ndarray):
+            return np.zeros_like(First) # un array numpy ne peut contenir de listes ou de array
+        else :
+            return First # C'est par exemple juste un float
 
 
-
-    def feedforward(self,A_K:[float],J:int): # entrée A_K : vecteur colonne des activations de la couche K, précédent la couche suivante J, on va jusqu'à la dernière couche récursivement
-        A_K = np.reshape(A_K,(len(A_K),1)) # transforme assurémenr en vecteur colonne
+    def feedforward(self,A_K:[float],J=1): # entrée A_K : vecteur colonne des activations de la couche K, précédent la couche suivante J, on va jusqu'à la dernière couche récursivement
+        # A_K = np.reshape(A_K,(len(A_K),1)) # transforme assurémenr en vecteur colonne ... en fait envoyer en entrée toujours des vecteurs colonnes
+        if isinstance(A_K,list) or len(A_K.shape) !=2 : return "erreur dans feedforward : entree A_K n'est pas un vecteur colonne"
         if J == len(self.taille) :
             return A_K
         else :
@@ -62,59 +83,65 @@ class Reseau():
         return [all_A,all_Z]
 
 
+    def gradient_local(self,entree,sortie_attendue): # entree et sortie_attendue doivent être des vecteurs colonnes
+        partial_derivatives = self.zeros_like_recursive(self.poids)
 
-
-    def gradient_local(self,entree,sortie_attendue):
-        partial_derivatives = np.zeros_like(self.poids)
         all_A, all_Z = self.all_activations(entree)
-        #partial derivatives_A
         sortie = all_A[-1]
 
-        par_der_A_S = [ 2 * (sortie[k] - sortie_attendue[k])**2   for k in range(len(sortie))   ]
+        par_der_A_S = np.array([ 2 * (sortie[k,0] - sortie_attendue[k,0])**2   for k in range(len(sortie))   ])[:,np.newaxis]
         def backpropagation(par_der_A_J,J):
+            K = J-1
             if J == 0 :
                 pass
             else :
-                par_der_A_K = np.zeros_like(all_A[J-1])
-                for j in range(self.taille[J]) :
-                    par_der_A_K += par_der_A_J[j] * self.der_activation_function(all_Z[J][j] * self.poids[J][0][j,:].T)
 
-                # Biais : b_j0 = drond A_j0 * sigma' z_j0 * 1
+                # Activations : d a_k0 = sum(i=1 à l(J) de drond_cout a_j * σ'(z_j) * w_jk0 )
+                par_der_A_K = np.zeros_like(all_A[K])
+                for j in range(len(par_der_A_J)) :
+                    par_der_A_K += par_der_A_J[j] * self.der_activation_function(all_Z[J][j]) * self.poids[J][0][j,np.newaxis].T
+
+                # Biais : d b_j0 = drond_cout a_j0 * σ'(z_j0) * 1
+                wup = partial_derivatives[J][1]
                 partial_derivatives[J][1] += par_der_A_J * self.der_activation_function(all_Z[J]) * 1
-                # Poids.T
-                for j_row
-                    partial_derivatives[J][0] +=  [j] * self.der_activation_function(all_Z[j]) * all_A[J-1].T    for j in range(self.taille[J]) for k in range(self.taille[J-1])
 
+                # Poids : d w_j0k0 = drond_cout a_j0 * σ'(z_j0) * a_k0
+                partial_derivatives[J][0] += all_A[K].T # On place d'abord a_k dans chaque colonne k
+                partial_derivatives[J][0] = par_der_A_J * self.der_activation_function(all_Z[J]) * partial_derivatives[J][0] # on multiplie ensuite chaque ligne j par drond a_j * σ'(z_j)
 
-
-
+                # Récursivement, on calcule maintenant les dérivées partielles des poids associés à la couche précédente (en s'appuyant sur les dérivées partielles des activations de cette couche)
                 backpropagation(par_der_A_K,J-1)
 
+        backpropagation(par_der_A_S,len(self.taille)-1)
+        return partial_derivatives
 
 
 
+    def apprendre(self,DATA:[ [[float],[float]] ],pas=1.0, data_shrink=1.0,data_number=None,precision=0,duration=99999,nb_generations=0,CHECK_PROPORTION=0.1):
+        if data_number is None : data_number = len(DATA)
+        gen_count,start_time,elapsed_time,manual_stop, DATA = -1,time(), 0, False, self.normalized(DATA)
+        # self.normalized permet de s'assurer que tout soit de la forme de vecteur colonne
+        if precision == 0 : CHECK_PROPORTION = -1
 
-
-    def apprendre(self,DATA:[ [[float],[float]] ],pas=1.0, data_shrink=1.0,data_number=None,precision=0,time=0,nb_generations=0,CHECK_PROPORTION=0.1):
-
-        gen_count,start_time,elapsed_time,manual_stop = -1,time(), 0, False
+        #rajouter la précision...
         def criteria_reached():
+            nonlocal gen_count
             if np.random.random() <= CHECK_PROPORTION :
                 self.evaluate_accuracy(DATA)
             gen_count += 1
             elapsed_time = time() - start_time
             # manual stop^with interface ?
 
-            return self.accuracy >= precision or gen_count >= nb_generations or elapsed_time >= time or manual_stop
-
+            return gen_count >= nb_generations or elapsed_time >= duration or manual_stop
 
         while not (criteria_reached()) :
 
             data = self.shuffled_data(DATA,data_shrink,data_number)
 
-            gradient_global = np.zeros_like(self.poids)
+            gradient_global = self.zeros_like_recursive(self.poids)
             for entree, sortie_attendue in data :
-                gradient_global += self.gradient_local(entree,sortie_attendue)
+                gradient_global += self.gradient_local(entree,sortie_attendue)  # soucis : s'il cpmprend les entrees de 0 et 1 comme des int, il peine à claculer
+            gradient_global = gradient_global # 'besoin' de prendre la moyenne mais pb multiplication
 
             self.poids += pas * gradient_global
 
@@ -129,14 +156,26 @@ class Reseau():
 my_reseau = Reseau([6,4,4,3])
 # print(my_reseau.poids)
 
-my_entree = np.array([0.7,0.4,0.6,0.8,0.1,0.1])
+entree1 = np.array([0.7,0.4,0.6,0.8,0.1,0.1])[:,np.newaxis]
+sortie_attendue1 = np.array([1,0,0])[:,np.newaxis]
 
-# print(my_entree.shape)
-# my_entree = my_entree[:,np.newaxis]
-# print(my_entree.shape)
+# print(entree1.shape)
+# entree1 = entree1[:,np.newaxis]
+# print(entree1.shape)
 
-print(my_reseau.feedforward(my_entree,1))
-print(my_reseau.all_activations(my_entree))
+# print(my_reseau.feedforward(entree1,1))
+# print(my_reseau.all_activations(entree1))
 
 # my_reseau.apprendre([True,False,False],100.5)
+
+# test_gradient_local1 = my_reseau.gradient_local(entree1,sortie_attendue1)
+# Résultat : le test a l'air concluant, ça marche maintenant !
+
+my_DATA = [ [[0,0,1.0,1,0,0],[0.0,1,0]], [[1,1,0,0.,0,0],[1.,0,0]], [[0.,0,0,0,1,1],[0.,0,1]]  ]
+
+# print(my_reseau.evaluate_accuracy(my_DATA,do_print=True))
+
+my_reseau.apprendre(my_DATA,nb_generations=100)
+
+
 
