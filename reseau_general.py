65513e4d9b9a744fe5a77fef13a88c74469ea083
GoldEEr17 <108426205+GoldEEr17@users.noreply.github.com>
2023-06-04 10:33:32 +0200

    Update reseau_general.py

--- reseau_general.py
+++ reseau_general.py
@@ -9,7 +9,6 @@ class Reseau():
     def __init__(self,taille:[int],act_function='sigmoid',input_parameters=None):
         self.taille = taille
         self.act_function = act_function
-        # self.poids = [[]] + [ [ np.random.uniform(-3,3,(taille[i],taille[i-1])),  np.random.uniform(-2,2,(taille[i],1))  ]      for i in range(1,len(self.taille)) ]
         self.weights = [[]] + [np.random.uniform(-3,3,(self.taille[i],self.taille[i-1])) for i in range(1,len(self.taille))]
         self.biases = [[]] + [np.random.uniform(-2,2,(self.taille[i],1)) for i in range(1,len(self.taille))]
         if not input_parameters is None :
@@ -43,13 +42,6 @@ class Reseau():
         nb_elements = min(data_number, data_shrink*len(DATA),)
         return random.sample(DATA,nb_elements)
 
-    # def op_lists(self,list1,op,list2):
-    #     if op == '+' :
-    #         return [x1 + x2 for x1,x2 in zip(list1,list2)]
-
-
-
-    #à corriger, et fonction de test à améliorer
     def evaluate_accuracy(self,data,do_print=False):
         data = self.normalized(data)
         correct_results = 0
@@ -64,17 +56,7 @@ class Reseau():
         self.accuracy = correct_results / len(data)
         if do_print==True : print(self.accuracy)
 
-    # def zeros_like_recursive(self,First):
-    #     if isinstance(First,list):
-    #         return [self.zeros_like_recursive(elem) for elem in First]
-    #     if isinstance(First,np.ndarray):
-    #         return np.zeros_like(First) # un array numpy ne peut contenir de listes ou de array
-    #     else :
-    #         return First # C'est par exemple juste un float
-
-
     def feedforward(self,A_K:[float],J=1): # entrée A_K : vecteur colonne des activations de la couche K, précédent la couche suivante J, on va jusqu'à la dernière couche récursivement
-        # A_K = np.reshape(A_K,(len(A_K),1)) # transforme assurémenr en vecteur colonne ... en fait envoyer en entrée toujours des vecteurs colonnes
         # if isinstance(A_K,list) or len(A_K.shape) !=2 : raise ValueError('PAS un vecteur colonne en entrée de feedforward')
         A_K = np.reshape(A_K,(-1,1))
         if J == len(self.taille) :
@@ -82,7 +64,7 @@ class Reseau():
         else :
             W_J = self.weights[J]
             B_J = self.biases[J]
-            Z_J = np.dot(W_J, A_K) + B_J #Commentaire : le produit matrice/vecteur se fait que A_K soit un vecteur colonne ou une liste isomorphe, pas de pb après
+            Z_J = np.dot(W_J, A_K) + B_J
             A_J = self.activation_function(Z_J)
 
             return self.feedforward(A_J,J+1)
@@ -93,7 +75,7 @@ class Reseau():
         while J < len(self.taille) :
             W_J = self.weights[J]
             B_J = self.biases[J]
-            Z_J = np.dot(W_J,A_K) + B_J  # Commentaire : le produit matrice/vecteur se fait que A_K soit un vecteur colonne ou une liste isomorphe, pas de pb après
+            Z_J = np.dot(W_J,A_K) + B_J  
             A_K = self.activation_function(Z_J)
 
             all_A.append(A_K)
@@ -103,7 +85,6 @@ class Reseau():
 
 
     def gradient_local(self,entree,sortie_attendue): # entree et sortie_attendue doivent être des vecteurs colonnes
-        # partial_derivatives = self.zeros_like_recursive(self.poids)
         entree = np.reshape(entree,(-1,1))
         sortie_attendue = np.reshape(sortie_attendue,(-1,1))
 
@@ -144,6 +125,7 @@ class Reseau():
         if DATA is None : DATA = self.stored_dataset
         if data_number is None : data_number = len(DATA)
         gen_count,start_time,elapsed_time, DATA,nb_seconds = 0,time(), 0, self.normalized(DATA),0
+        if duration == 999999 and nb_generations == 999999 : raise Exception("pas de fin d'apprentissage !")
         # self.normalized permet de s'assurer que tout soit de la forme de vecteur colonne
         # if precision == 0 : CHECK_PROPORTION = -1
 
@@ -157,7 +139,7 @@ class Reseau():
             if int(elapsed_time) > nb_seconds : # évalue l'accuracy 1 fois seulement par seconde
                 nb_seconds = int(elapsed_time)
                 self.evaluate_accuracy(DATA)
-                print(f"accuracy à {nb_seconds}s : {self.accuracy}")
+                # print(f"accuracy à {nb_seconds}s : {self.accuracy}")
 
             return gen_count >= nb_generations or elapsed_time >= duration
 
@@ -185,119 +167,5 @@ class Reseau():
         self.total_duration += nb_seconds
         print(f'\nnb total générations : {self.total_generations} | durée : {elapsed_time:.0f}s')
 
-
-
-
-
-my_reseau = Reseau([6,4,4,3])
-# my_reseau = Reseau([6,50,4,3])
-
-# my_reseau = Reseau([6,3])
-# print(my_reseau.poids)
-
-entree1 = np.array([0.7,0.4,0.6,0.8,0.1,0.1])[:,np.newaxis]
-sortie_attendue1 = np.array([1,0,0])[:,np.newaxis]
-
-# gradient_local1_w,gradient_local1_b = my_reseau.gradient_local(entree1,sortie_attendue1)
-# print(f'gradient_local1 = {gradient_local1_w,gradient_local1_b}')
-# print(entree1.shape)
-# entree1 = entree1[:,np.newaxis]
-# print(entree1.shape)
-
-# print(my_reseau.feedforward(entree1,1))
-# print(my_reseau.all_activations(entree1))
-
-# my_reseau.apprendre([True,False,False],100.5)
-
-# test_gradient_local1 = my_reseau.gradient_local(entree1,sortie_attendue1)
-# Résultat : le test a l'air concluant, ça marche maintenant !
-
-my_DATA = [ [[0,0,1.0,1,0,0],[0.0,1,0]], [[1,1,0,0.,0,0],[1.,0,0]], [[0.,0,0,0,1,1],[0.,0,1]]  ]
-# my_DATA = [ [[0.,0.,1.,1.,0.,0.],[0.,1.,0.]] ]
-# my_DATA = [ [[0,0,1,1,0,0],[0,1,0]] ]
-
-
-# my_DATA = [ [[0.,0,1,1,0,0],[0,1,0]], [[1,1,0,0,0,0],[1,0,0]], [[0,0,0,0,1,1],[0,0,1]]  ]
-
-# print(my_reseau.evaluate_accuracy(my_DATA,do_print=True))
-
-# my_reseau.evaluate_accuracy(my_DATA,do_print=True)
-# # my_reseau.apprendre(my_DATA,nb_generations=500,pas=10.0)
-# my_reseau.apprendre(my_DATA,duration=5,pas=10.0)
-#
-#
-# print(my_reseau.feedforward([0.,0.,1.,1.,0.,0.])) #ça marche!!!!
-#
-# print(my_reseau.feedforward([1,1,0,0,0,0]))
-# my_reseau.evaluate_accuracy(my_DATA,do_print=True)
-
-
-# my_reseau.apprendre(my_DATA,duration=5,pas=10.0)
-# with open('1st_storage_test','wb') as file :
-#     pickle.dump(my_reseau.weights, file)
-#     pickle.dump(my_reseau.biases, file)
-#     pickle.dump(my_reseau, file)
-
-
-# with open('1st_storage_test','rb') as file :
-#     w_stored = pickle.load(file)
-#     b_stored = pickle.load(file)
-#     reseau_stored = pickle.load(file)
-#
-# opened_reseau1 = Reseau([6,4,4,3], input_parameters=[w_stored,b_stored])
-# opened_reseau2 = reseau_stored
-#
-# opened_reseau2.stored_dataset = my_DATA
-# opened_reseau2.apprendre(duration=15)
-#
-# with open('2nd_storage_test','wb') as file :
-#     pickle.dump(opened_reseau2,file)
-#
-#
-#
-# pass
-
-
-# with open('2nd_storage_test','rb') as file :
-#     opened_reseau3 = pickle.load(file)
-#
-# pass
-#
-# reseau_actuel = opened_reseau3
-# reseau_actuel.apprendre(duration=30)
-#
-
-def save(reseau) :
-    with open('test_latest_saved','wb') as file :
-        pickle.dump(reseau, file)
-
-def loaded() :
-    with open('test_latest_saved','rb') as file :
-        return pickle.load(file)
-
-
-## LEARNING PHASE
-if False :
-    with open('test_latest_saved','rb') as file :
-        reseau_actuel = pickle.load(file)
-
-    reseau_actuel.apprendre(duration=5*60)
-    # reseau_actuel.stored_dataset = my_DATA
-
-
-
-
-    with open('test_latest_saved','wb') as file :
-        pickle.dump(reseau_actuel,file)
-
-
-
-## CHECKING PHASE
-with open('test_latest_saved','rb') as file :
-    reseau_actuel = pickle.load(file)
-    pass
-
-
-print(reseau_actuel.feedforward(my_DATA[0][0]))
 
 
